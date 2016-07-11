c2d69bdb5d7a61bd21220344fd6525900ac1481d
Yoshi Oe <ysfmmm@gmail.com>
2016-07-11 20:58:17 +0900

    before

---
+++ JavaSample/src/refactoring/sample/main.java
@@ -0,0 +1,25 @@
+package refactoring.sample;
+
+public class main {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer("Joe");
+
+		Movie movie = new Movie("Doraemon", 1);
+		Rental rental = new Rental(movie, 10);
+
+		Movie movie2 = new Movie("Konan", 2);
+		Rental rental2 = new Rental(movie2, 3);
+
+		Movie movie3 = new Movie("Dragon Ball", 0);
+		Rental rental3 = new Rental(movie3, 100);
+
+		customer.addRental(rental);
+		customer.addRental(rental2);
+		customer.addRental(rental3);
+
+		System.out.println(customer.statement());
+
+	}
+
+}
