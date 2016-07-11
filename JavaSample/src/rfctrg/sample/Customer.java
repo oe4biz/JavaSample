a0947d927b6f47ee428922690638f784c4f659cd
Yoshi Oe <ysfmmm@gmail.com>
2016-07-11 21:24:41 +0900

    Customerのリファクタリング

--- JavaSample/src/rfctrg/sample/Customer.java
+++ JavaSample/src/rfctrg/sample/Customer.java
@@ -1,18 +1,27 @@
 package rfctrg.sample;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Vector;
+
+import rfctrg.sample.Rental;
 
 public class Customer {
 	private String _name;
-	private Vector _rentals = new Vector<String>();
+	private List<Rental> _rentals;
 
 	public Customer(String name){
 		_name = name;
+		_rentals = new ArrayList<Rental>();
 	}
 
 	public void addRental(Rental rental) {
 		_rentals.add(rental);
+	}
+
+	public void addRentals(List<Rental> rentals) {
+		_rentals = rentals;
 	}
 
 	public String getName() {
@@ -20,51 +29,51 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
 		String result = "Rental Record for " + getName() + "\n";
-		Enumeration rentals = _rentals.elements();
-
-		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
-			Rental each = (Rental) rentals.nextElement();
-
-			//1行ごとに金額計算
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount = 2;
-				if (each.getDaysRented() > 2) {
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				}
-				break;
-
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented()  * 3;
-
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3) {
-					thisAmount += (each.getDaysRented() -3 ) * 1.5;
-				}
-
-				break;
-			}
-
-			//レンタルポイント加算
-			frequentRenterPoints++;
-			//新作を二日以上借りた場合はボーナスポイント
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-					each.getDaysRented() > 1) frequentRenterPoints++;
+		//Enumeration rentals = _rentals.elements();
+		for (Rental rental : this._rentals) {
 
 			//この貸し出しに関する数値の表示
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
 		}
 
 		//フッタ部分の追加
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+		result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
+
+		return result;
+	}
+
+	public String htmlStatement() {
+		String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><p>\n";
+		for (Rental rental : this._rentals) {
+			//この貸し出しに関する数値の表示
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "<BR>\n";
+		}
+
+		//フッタ部分の追加
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+
+		return result;
+	}
+
+	public double getTotalCharge() {
+		double result = 0;
+
+		for (Rental rental : this._rentals) {
+			result += rental.getCharge();
+		}
+
+		return result;
+	}
+
+	public int getFrequentRenterPoints() {
+		int result = 0;
+
+		for (Rental rental : this._rentals) {
+			result += rental.getCharge();
+		}
 
 		return result;
 	}
