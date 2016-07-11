70713d050be326bbae4939f3087e71ff43682b02
Yoshi Oe <ysfmmm@gmail.com>
2016-07-11 22:36:19 +0900

    Main????

--- JavaSample/src/rfctrg/second/Main.java
+++ JavaSample/src/rfctrg/second/Main.java
@@ -3,8 +3,8 @@ package rfctrg.second;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("test1");
+		System.out.println("test2");
 	}
 
 }
